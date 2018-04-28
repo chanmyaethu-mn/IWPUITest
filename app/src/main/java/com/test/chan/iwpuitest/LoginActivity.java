9e1953ba0a88f67c1d26476f7e9ec82d5659b7fb
Chan Myae Thu <chanmyaethu.mn@gmail.com>
2018-04-28 16:38:06 +0800

    add login form

--- app/src/main/java/com/test/chan/iwpuitest/LoginActivity.java
+++ app/src/main/java/com/test/chan/iwpuitest/LoginActivity.java
@@ -2,12 +2,20 @@ package com.test.chan.iwpuitest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.Fullscreen;
+import org.androidannotations.annotations.WindowFeature;
+
+@Fullscreen
+@EActivity(R.layout.activity_login)
+@WindowFeature(Window.FEATURE_NO_TITLE)
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        //setContentView(R.layout.activity_login);
     }
 }
